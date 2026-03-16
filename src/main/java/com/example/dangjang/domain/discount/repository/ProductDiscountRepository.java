6e9f17c6c6ed0fe89ff8ee69433a587ea759f84e
LeoOH5 <bbin0409@naver.com>
2026-03-17 00:05:30 +0900

    feat : 상품, 할인상품 repository 추가

---
+++ src/main/java/com/example/dangjang/domain/discount/repository/ProductDiscountRepository.java
@@ -0,0 +1,15 @@
+package com.example.dangjang.domain.discount.repository;
+
+import com.example.dangjang.domain.discount.entity.DiscountStatus;
+import com.example.dangjang.domain.discount.entity.ProductDiscount;
+import com.example.dangjang.domain.product.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface ProductDiscountRepository extends JpaRepository<ProductDiscount, Long> {
+
+    List<ProductDiscount> findByProductAndStatus(Product product, DiscountStatus status);
+}
+
---
+++ src/main/java/com/example/dangjang/domain/product/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.dangjang.domain.product.repository;
+
+import com.example.dangjang.domain.product.entity.Product;
+import com.example.dangjang.domain.store.entity.Store;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByStore(Store store);
+}
+
