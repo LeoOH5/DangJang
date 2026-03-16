f6777d9e9c744ab2085ede35eedd4e92ac4bd6f5
LeoOH5 <bbin0409@naver.com>
2026-03-17 00:34:08 +0900

    feat : 매장 수정 요청, 응답 dto 추가

---
+++ src/main/java/com/example/dangjang/domain/store/dto/StoreUpdateResponse.java
@@ -0,0 +1,12 @@
+package com.example.dangjang.domain.store.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class StoreUpdateResponse {
+
+    private final Long storeId;
+}
+
