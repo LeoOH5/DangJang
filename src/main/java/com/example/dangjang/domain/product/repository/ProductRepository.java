2e1a5fc0413ef8cd9ed50984bd60bdb1eb3016ef
LeoOH5 <bbin0409@naver.com>
2026-03-18 16:20:52 +0900

    feat : 상품 repository 메소드 추가

--- src/main/java/com/example/dangjang/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/dangjang/domain/product/repository/ProductRepository.java
@@ -9,5 +9,7 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findByStore(Store store);
+
+    boolean existsByStoreIdAndName(Long storeId, String name);
 }
 
