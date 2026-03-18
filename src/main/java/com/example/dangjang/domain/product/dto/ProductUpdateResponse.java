a997bce966ecc7b3ccdf5d8d8ed2e2d413e356e6
LeoOH5 <bbin0409@naver.com>
2026-03-18 17:46:30 +0900

    feat : 상품 수정 요청, 응답 dto 추가

---
+++ src/main/java/com/example/dangjang/domain/product/dto/ProductUpdateResponse.java
@@ -0,0 +1,12 @@
+package com.example.dangjang.domain.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ProductUpdateResponse {
+
+    private final Long productId;
+}
+
