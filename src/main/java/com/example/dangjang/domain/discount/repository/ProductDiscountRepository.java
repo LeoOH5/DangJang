9257d3ad84392f964bf5acacf1926cbf572ddf9a
LeoOH5 <bbin0409@naver.com>
2026-03-19 13:00:31 +0900

    add : 상품 할인 관련 repository 메소드 추가

--- src/main/java/com/example/dangjang/domain/discount/repository/ProductDiscountRepository.java
+++ src/main/java/com/example/dangjang/domain/discount/repository/ProductDiscountRepository.java
@@ -5,11 +5,12 @@ import com.example.dangjang.domain.discount.entity.ProductDiscount;
 import com.example.dangjang.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ProductDiscountRepository extends JpaRepository<ProductDiscount, Long> {
 
     List<ProductDiscount> findByProductAndStatus(Product product, DiscountStatus status);
+
+    List<ProductDiscount> findByProduct(Product product);
 }
 
