e6a1bececd9b60315241a80214a1e3934a4af09d
LeoOH5 <bbin0409@naver.com>
2026-03-24 01:10:37 +0900

    add: repository 메소드 추가

--- src/main/java/com/example/dangjang/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/dangjang/domain/product/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.example.dangjang.domain.product.repository;
 
 import com.example.dangjang.domain.product.entity.Product;
+import com.example.dangjang.domain.product.entity.ProductStatus;
 import com.example.dangjang.domain.store.entity.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,5 +14,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByStore(Store store);
 
     boolean existsByStoreIdAndName(Long storeId, String name);
+
+    Page<Product> findByNameContainingIgnoreCaseAndStatusNot(String keyword, ProductStatus status, Pageable pageable);
 }
 
