0025b1993a4db329fe69b41141e1b7a2277bcb54
LeoOH5 <bbin0409@naver.com>
2026-03-23 19:12:37 +0900

    add : 할인 상품 조회 페이징 함수 추가

--- src/main/java/com/example/dangjang/domain/discount/repository/ProductDiscountRepository.java
+++ src/main/java/com/example/dangjang/domain/discount/repository/ProductDiscountRepository.java
@@ -4,13 +4,31 @@ import com.example.dangjang.domain.discount.entity.DiscountStatus;
 import com.example.dangjang.domain.discount.entity.ProductDiscount;
 import com.example.dangjang.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.time.LocalDateTime;
 
 public interface ProductDiscountRepository extends JpaRepository<ProductDiscount, Long> {
 
     List<ProductDiscount> findByProductAndStatus(Product product, DiscountStatus status);
 
     List<ProductDiscount> findByProduct(Product product);
+
+    @Query("""
+            select pd
+            from ProductDiscount pd
+            where pd.startAt <= :now
+              and pd.endAt >= :now
+              and pd.remainingQuantity > 0
+            order by pd.id desc
+           """)
+    Page<ProductDiscount> findActiveDiscountProducts(
+            @Param("now") LocalDateTime now,
+            Pageable pageable
+    );
 }
 
