67d404dd4660bfb2f42f80df6e7c50a5236c3415
LeoOH5 <bbin0409@naver.com>
2026-03-23 19:06:48 +0900

    add : 할인 상품 관련 enum 값 추가

--- src/main/java/com/example/dangjang/domain/discount/entity/DiscountType.java
+++ src/main/java/com/example/dangjang/domain/discount/entity/DiscountType.java
@@ -2,5 +2,26 @@ package com.example.dangjang.domain.discount.entity;
 
 public enum DiscountType {
     AMOUNT,
-    PERCENT
+    PERCENT,
+
+    FIXED_AMOUNT,
+    PERCENTAGE;
+
+    public boolean isPercent() {
+        return this == PERCENT || this == PERCENTAGE;
+    }
+
+    public boolean isAmount() {
+        return this == AMOUNT || this == FIXED_AMOUNT;
+    }
+
+    public DiscountType normalize() {
+        if (isPercent()) {
+            return PERCENT;
+        }
+        if (isAmount()) {
+            return AMOUNT;
+        }
+        return this;
+    }
 }
