6e8d2f6ae087c546571f9a93b85285bd7b519726
LeoOH5 <bbin0409@naver.com>
2026-03-28 01:50:24 +0900

    add : repository 메소드 추가

--- src/main/java/com/example/dangjang/domain/favorite/repository/FavoriteRepository.java
+++ src/main/java/com/example/dangjang/domain/favorite/repository/FavoriteRepository.java
@@ -3,6 +3,8 @@ package com.example.dangjang.domain.favorite.repository;
 import com.example.dangjang.domain.favorite.entity.Favorite;
 import com.example.dangjang.domain.store.entity.Store;
 import com.example.dangjang.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -12,5 +14,7 @@ public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
     boolean existsByUserAndStore(User user, Store store);
 
     Optional<Favorite> findByUserAndStore(User user, Store store);
+
+    Page<Favorite> findByUser(User user, Pageable pageable);
 }
 
