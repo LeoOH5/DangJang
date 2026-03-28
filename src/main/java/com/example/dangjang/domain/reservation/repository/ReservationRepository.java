1372adb9aaabf9bc9494396c922d40afbbc6235c
LeoOH5 <bbin0409@naver.com>
2026-03-28 19:32:45 +0900

    add 예약 repository 페이징 기능 추가

--- src/main/java/com/example/dangjang/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/dangjang/domain/reservation/repository/ReservationRepository.java
@@ -1,8 +1,18 @@
 package com.example.dangjang.domain.reservation.repository;
 
 import com.example.dangjang.domain.reservation.entity.Reservation;
+import com.example.dangjang.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @EntityGraph(attributePaths = {"store"})
+    Page<Reservation> findByUserOrderByIdDesc(User user, Pageable pageable);
+
+    @EntityGraph(attributePaths = {"store"})
+    Page<Reservation> findByUserAndStatusOrderByIdDesc(User user, String status, Pageable pageable);
 }
 
