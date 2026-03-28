c0c762aad77944905d7dd2548dae4f6b2fefddd4
LeoOH5 <bbin0409@naver.com>
2026-03-28 19:47:06 +0900

    add : repository에 메소드 추가

--- src/main/java/com/example/dangjang/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/example/dangjang/domain/reservation/repository/ReservationRepository.java
@@ -6,6 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
@@ -14,5 +18,15 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
     @EntityGraph(attributePaths = {"store"})
     Page<Reservation> findByUserAndStatusOrderByIdDesc(User user, String status, Pageable pageable);
+
+    @Query("""
+            select distinct r from Reservation r
+            left join fetch r.user
+            left join fetch r.store
+            left join fetch r.items i
+            left join fetch i.product
+            where r.id = :id
+            """)
+    Optional<Reservation> findDetailById(@Param("id") Long id);
 }
 
