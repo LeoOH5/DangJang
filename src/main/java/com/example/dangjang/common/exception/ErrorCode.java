b6e7a4866a5e3abefae0cdfa6f06f50b9e24d9a4
LeoOH5 <bbin0409@naver.com>
2026-04-06 03:13:30 +0900

    add : 매장 검색 에러코드 추가

--- src/main/java/com/example/dangjang/common/exception/ErrorCode.java
+++ src/main/java/com/example/dangjang/common/exception/ErrorCode.java
@@ -8,6 +8,7 @@ public enum ErrorCode {
 
     // Common
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다."),
+    INVALID_SEARCH_CONDITION(HttpStatus.BAD_REQUEST, "INVALID_SEARCH_CONDITION", "검색 조건이 올바르지 않습니다."),
     METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "COMMON_405", "허용되지 않은 HTTP 메서드입니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 내부 오류가 발생했습니다."),
 
