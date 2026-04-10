e1e977d0684f0024858a2bc460ee35fb8e750ad3
LeoOH5 <bbin0409@naver.com>
2026-04-10 19:25:05 +0900

    feat : 리뷰 수정 관련 요청, 응답 dto 추가

---
+++ src/main/java/com/example/dangjang/domain/review/dto/ReviewUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.example.dangjang.domain.review.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ReviewUpdateRequest {
+
+    private Integer rating;
+    private String content;
+}
+
