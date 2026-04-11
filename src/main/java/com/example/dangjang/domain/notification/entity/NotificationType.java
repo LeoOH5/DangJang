998fba7ef8751735325becf05377bbf283dac560
LeoOH5 <bbin0409@naver.com>
2026-04-11 17:12:33 +0900

    fix : 알림 이벤트 종류 수정

--- src/main/java/com/example/dangjang/domain/notification/entity/NotificationType.java
+++ src/main/java/com/example/dangjang/domain/notification/entity/NotificationType.java
@@ -1,15 +1,13 @@
 package com.example.dangjang.domain.notification.entity;
 
-enum NotificationType {
+public enum NotificationType {
     // 예약 관련
-    RESERVATION_REQUESTED,      // 사용자 → 매장
-    RESERVATION_CONFIRMED,      // 매장 → 사용자
-    RESERVATION_REJECTED,       // 매장 → 사용자
-
+    RESERVATION_REQUESTED,
+    RESERVATION_CONFIRMED,
+    RESERVATION_REJECTED,
     // 할인 / 상품
-    DISCOUNT_CREATED,           // 매장 → 사용자 (즐겨찾기 기반)
-
+    DISCOUNT_CREATED,
     // 시스템 알림
-    PICKUP_REMINDER,            // 시스템 → 사용자
+    PICKUP_REMINDER,
+    POPULAR_PRODUCT_RECOMMENDATION,
 }
-
